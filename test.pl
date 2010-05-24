407fa0c26750e3c2beef9b91a6c92bbad59c8efa
Chris Marshall <devel.chm.01@gmail.com>
2010-05-24 05:51:19 -0400

    Remove use Math::Trig from test.pl
    No methods/subs were apparently being used from that module.
    This avoids a conflict with PDL.

--- test.pl
+++ test.pl
@@ -18,7 +18,12 @@ my $hasShader = !$@;
 eval 'use Image::Magick';
 my $hasIM = !$@;
 
-use Math::Trig;
+# This does not seem to be needed and it adds an extra, unneeded
+# dependency to the build process.  Leaving this in as a comment
+# just in case it is being used somewhere here
+#
+# use Math::Trig;
+
 eval 'use Time::HiRes qw( gettimeofday )';
 my $hasHires = !$@;
 $|++;
