c2d0388d87f246e88bb86ca989aa423ce660c1dd
Chris Marshall <devel.chm.01@gmail.com>
2016-05-24 07:56:08 -0400

    Update readme type docs and copyright notices

--- OpenGL.pm
+++ OpenGL.pm
@@ -4,6 +4,7 @@ package OpenGL;
 #  Copyright (c) 2007 Bob Free. All rights reserved.
 #  Copyright (c) 2009 Christopher Marshall. All rights reserved.
 #  Copyright (c) 2015 Bob Free. All rights reserved.
+#  Copyright (c) 2016 Chris Marshall. All rights reserved.
 #  This program is free software; you can redistribute it and/or
 #  modify it under the same terms as Perl itself.
 
